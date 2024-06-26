49df9e90224a8d03377e75cbf4bec1f43fde7f29
Aleksey <118374395+SenseyAleksey@users.noreply.github.com>
2024-06-26 08:05:34 +0000

    fourth commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите выражение (например, 'X + X'): ");
-        String input = scanner.nextLine();
 
-        try {
-            String result = Calculator.calc(input);
-            System.out.println("Результат: " + result);
-        } catch (Exception e) {
-            System.out.println("Ошибка: " + e.getMessage());
+        while (true) {
+            System.out.println("Введите выражение (например, 'X + X') или 'exit' для завершения: ");
+            String input = scanner.nextLine();
+
+            // Проверка на команду выхода
+            if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Программа завершена.");
+                break;
+            }
+
+            try {
+                String result = Calculator.calc(input);
+                System.out.println("Результат: " + result);
+            } catch (Exception e) {
+                System.out.println("Ошибка: " + e.getMessage());
+            }
         }
 
         scanner.close();
